76b6c2fcda7cc08415208e951f4cf90d64beebde
furPedroRibeiro <pedrocodejob@gmail.com>
2024-07-21 12:19:08 -0300

    configurações feitas, projeto iniciado

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <>
+      <main className="flex flex-col items-center justify-center px-10">
+        <h1 className="text-white font-bold text-2xl py-7 text-center">Bem vindo ao Matemática e Física</h1>
+        <p className="text-white font-regular text-lg text-center">Aqui você encontra calculadoras de temas específicos para usar como e quando quiser!! Este projeto é desenvolvido por um aluno de Egenharia de computação da USP de São Carlos, fique a vontade para dar seu feedback. Informações de contato estão no rodapé da página.</p>
+      </main>
+    </>
+  )
+}
